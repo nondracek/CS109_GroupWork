f345f9f5f494a85a401f07fcfdd2ddf639b2ec73
Nathan Ondracek <nondracek@Nathans-MacBook-Pro-5.local>
2017-11-24 19:54:21 -0500

    Added More Playlists

--- Final Project/SpotifyTest.py
+++ Final Project/SpotifyTest.py
@@ -2,6 +2,8 @@ import sys
 import spotipy
 import spotipy.util as util
 import pandas as pd
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 scope = 'user-library-read'
 
@@ -18,14 +20,19 @@ else:
 token = util.prompt_for_user_token(username, scope, client_id='c4bfe10bf6304a8ba3e075e878ec5ddf',client_secret='0da11797bf604e32b624cf82cea5600a',redirect_uri='SpotifyData://returntoapp')
 
 sp = spotipy.Spotify(auth = token)
-playlists = sp.featured_playlists()
+playlists = sp.featured_playlists()['playlists']['items']
+playlists = playlists + sp.search("Country", 20, offset=0, type = 'playlist')['playlists']['items']
+playlists = playlists + sp.search("Rap", 20, offset=0, type = 'playlist')['playlists']['items']
+playlists = playlists + sp.search("Jazz", 20, offset=0, type = 'playlist')['playlists']['items']
+playlists = playlists + sp.search("Oldies", 20, offset=0, type = 'playlist')['playlists']['items']
+playlists = playlists + sp.search("Rock", 20, offset=0, type = 'playlist')['playlists']['items']
 # playlists = sp.user_playlists(username)
 # playlistName = playlists['playlists']['items'][1]['id']
 
 # # playlist = playlists['items'][0]['id']
 # results = sp.search(q=query, limit=limit, type='playlist')
 stuff = []
-for playlist in playlists['playlists']['items']:
+for playlist in playlists:
     # results = sp.user_playlist(playlist['owner']['id'], playlist['id'], fields="tracks,next")
     results = sp.user_playlist(playlist['owner']['id'], playlist['id'])
     playlistInfo = [results['name']]
@@ -39,13 +46,15 @@ for playlist in playlists['playlists']['items']:
         averageSongPopularity += float(track['track']['popularity'])/float(len(results['tracks']['items']))
         howExplicit += float(track['track']['explicit'])/float(len(results['tracks']['items']))
         averageSongLength += float(track['track']['duration_ms'])/float(len(results['tracks']['items']))
-        artists.append(track['track']['album']['artists'][0]['name'])
-    print artists
+        # if track['track']['album']['artists'][0]['name'].decode('string_escape'):
+        if len(track['track']['album']['artists']) > 0:
+            artists.append(track['track']['album']['artists'][0]['name'].encode('utf-8'))
     playlistInfo.append(averageSongPopularity)
     playlistInfo.append(howExplicit)
     playlistInfo.append(averageSongLength)
     playlistInfo.append(artists)
     stuff.append(playlistInfo)
-    DataFrame = pd.DataFrame(stuff)
-    DataFrame.columns = ['Name', 'Followers', 'NumTracks', 'Avg Song Pop', 'Explicitness', 'Avg Song Len', 'Artist']
-    DataFrame.to_csv('SpotifyData')
+DataFrame = pd.DataFrame(stuff)
+DataFrame.columns = ['Name', 'Followers', 'NumTracks', 'Avg Song Pop', 'Explicitness', 'Avg Song Len', 'Artists']
+print DataFrame
+DataFrame.to_csv('SpotifyData')
